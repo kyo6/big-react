7dd0388a290efd041143eabb0338a176ddbd0054
qiang.liu <qiang.liu@huifu.com>
2025-11-07 13:10:30 +0800

    feat: 实现jsx的转换以及打包流程

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,70 @@
+import { REACT_ELEMENT_TYPE } from "shared/ReactSymbols";
+import {
+  Type,
+  Key,
+  Ref,
+  Props,
+  ElementType,
+  ReactElementType,
+} from "shared/ReactTypes";
+
+// ReactElement
+
+export const ReactElement = function (
+  type: Type,
+  key: Key,
+  ref: Ref,
+  props: Props
+): ReactElementType {
+  const element = {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type,
+    key,
+    ref,
+    props,
+    _mark: "lsh",
+  };
+
+  return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+  // reactElement 自身的属性
+  let key: Key = null;
+  const props: Props = {};
+  let ref: Ref = null;
+
+  for (const prop in config) {
+    const val = config[prop];
+    if (prop === "key") {
+      if (val !== undefined) {
+        key = "" + val;
+      }
+      continue;
+    }
+    if (prop === "ref") {
+      if (val !== undefined) {
+        ref = val;
+      }
+      continue;
+    }
+    // 去除config原型链上的属性，只要自身
+    if ({}.hasOwnProperty.call(config, prop)) {
+      props[prop] = val;
+    }
+  }
+
+  const maybeChildrenLength = maybeChildren.length;
+  if (maybeChildrenLength) {
+    // [child] [child, child, child]
+    if (maybeChildrenLength === 1) {
+      props.children = maybeChildren[0];
+    } else {
+      props.children = maybeChildren;
+    }
+  }
+
+  return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
